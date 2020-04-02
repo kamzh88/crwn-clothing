4f3767c8760d168f7b4aff23b9c95ccdcdb196f8
KinKam Zhao <kamzh88@hotmail.com>
2020-04-02 15:55:09 -0400

    deploy

---
+++ client/build/precache-manifest.be73c0b9c2638d4bea08b7aea3a53991.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45a1f187a16760801b923aba59a08c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e06ef6c5556777cee05e",
+    "url": "/static/css/main.2a57ff6d.chunk.css"
+  },
+  {
+    "revision": "5e97e7dd8f83a94c3aa4",
+    "url": "/static/js/2.55de3a30.chunk.js"
+  },
+  {
+    "revision": "6985d75ffd76ce4f1484f26e7c7f1ad2",
+    "url": "/static/js/2.55de3a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06ef6c5556777cee05e",
+    "url": "/static/js/main.ccb8bb22.chunk.js"
+  },
+  {
+    "revision": "405f430893446efaf6a2",
+    "url": "/static/js/runtime-main.9129b20f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
